b5c5bdf5372b43a59aa92ea0adc3a806a3313e12
Saad-Ali-Khan-007 <saadalikhan042@gmail.com>
2023-08-06 19:00:58 +0500

    Learning React

---
+++ src/Content.js
@@ -0,0 +1,14 @@
+const Content = () => {
+    const handleNameChange = () => {
+        const names = ['Jack','Bob',"Carl","Saad"]
+        const int = Math.floor(Math.random()*4)
+        return names[int]
+    }
+    return (
+        <main>
+            <p>Hello {handleNameChange()}!</p>
+        </main>
+    )
+}
+
+export default Content
